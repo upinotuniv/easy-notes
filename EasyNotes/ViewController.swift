d9a451df05415492e31d1f70376db6267d37f61a
LuthfyRa.1204 <Luthfy.Rahmani@mii.co.id>
2023-08-28 14:49:51 +0700

    Initial Commit

---
+++ EasyNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyNotes
+//
+//  Created by PRO M1 2020 8/256 on 28/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
